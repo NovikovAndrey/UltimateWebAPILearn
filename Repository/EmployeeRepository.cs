31ebbeeb40ad129ad48e6992334509348970701b
Андрей Новиков <novikov.andrey.v@hotmail.com>
2021-03-14 13:50:56 +0300

    add repoClasses

---
+++ Repository/EmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using Contracts.Interfaces.Entities;
+using Entities;
+using Entities.Models;
+
+namespace Repository
+{
+    public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(RepositoryContext repositoryContext)
+            :base(repositoryContext)
+        {
+
+        }
+    }
+}
