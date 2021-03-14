8c936f26431a6b0b6801148e310fafa90eebb84a
Андрей Новиков <novikov.andrey.v@hotmail.com>
2021-03-14 21:52:08 +0300

    Add Employee

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Contracts.Interfaces.Entities;
 using Entities;
 using Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -18,6 +19,11 @@ namespace Repository
         {
             return FindAll(trackChanges).OrderBy(c => c.Name).ToList();
 
+        }
+
+        public Company GetCompany(Guid companyId, bool trackChanges)
+        {
+            return FindByCondition(c => c.Id.Equals(companyId), trackChanges).SingleOrDefault();
         }
     }
 }
