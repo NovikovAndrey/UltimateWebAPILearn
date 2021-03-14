3b048065c9115ca51589c0816d618826b4f281ef
Андрей Новиков <novikov.andrey.v@hotmail.com>
2021-03-14 14:42:52 +0300

    Add first controller

---
+++ UltimateWebAPILearn/Controllers/CompaniesController.cs
@@ -0,0 +1,36 @@
+﻿using Contracts.Interfaces.Entities;
+using Contracts.Interfaces.Logging;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace UltimateWebAPILearn.Controllers
+{
+    [Route("api/companies")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+
+        public CompaniesController(IRepositoryManager repository, ILoggerManager logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetCompanies()
+        {
+            try
+            {
+                var companies = _repository.Company.GetAllCompanies(false);
+                return Ok(companies);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetCompanies)} action {ex}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
