753f4ede674fbfacd9e911dbf77eb3f75bed1c19
Андрей Новиков <novikov.andrey.v@hotmail.com>
2021-03-14 12:28:26 +0300

    Add testdata

--- Entities/RepositoryContext.cs
+++ Entities/RepositoryContext.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Models;
+﻿using Entities.Configuration;
+using Entities.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Entities
@@ -10,6 +11,12 @@ namespace Entities
             :base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
         }
 
         public DbSet<Company> Companies { get; set; }
