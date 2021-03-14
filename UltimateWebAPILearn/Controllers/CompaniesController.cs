8361b4d306446ce077fb722b4aaeb99609181f49
Андрей Новиков <novikov.andrey.v@hotmail.com>
2021-03-14 15:44:03 +0300

    fix mspper

--- UltimateWebAPILearn/Controllers/CompaniesController.cs
+++ UltimateWebAPILearn/Controllers/CompaniesController.cs
@@ -27,17 +27,8 @@ namespace UltimateWebAPILearn.Controllers
         [HttpGet]
         public IActionResult GetCompanies()
         {
-            try
-            {
-                var companies = _repository.Company.GetAllCompanies(false);
-                var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
-                return Ok(companiesDto);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Something went wrong in the {nameof(GetCompanies)} action {ex}");
-                return StatusCode(500, "Internal server error");
-            }
+            var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(_repository.Company.GetAllCompanies(false));
+            return Ok(companiesDto);
         }
     }
 }
